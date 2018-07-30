d7617adb9ae7311d046a69f1c45ae4932d809866
Tero Tapio <teromarkustapio@gmail.com>
2018-07-30 18:08:13 +0300

    adds universal links to pages, points can be added to teams

--- application/teams/views.py
+++ application/teams/views.py
@@ -10,6 +10,15 @@ def teams_index():
 def teams_form():
     return render_template("teams/new.html")
 
+@app.route("/teams/<team_id>/", methods=["POST"])
+def teams_add_point(team_id):
+
+    t = Team.query.get(team_id)
+    t.points = t.points + 1
+    db.session().commit()
+
+    return redirect(url_for("teams_index"))
+
 @app.route("/teams/", methods=["POST"])
 def teams_create():
     t = Team(request.form.get("name"))
